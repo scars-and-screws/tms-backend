e4da25946c8c58dad4efc2c881fc81ead8356530
scars-and-screws <faridaaa58@gmail.com>
2026-04-24 14:53:27 +0530

    organization logo parm schema update

--- src/modules/organizations/logo/logo.validation.js
+++ src/modules/organizations/logo/logo.validation.js
@@ -3,6 +3,6 @@ import { idSchema } from "../../../core/validation/index.js";
 // ! ORGANIZATION LOGO PARAM VALIDATION SCHEMA
 export const organizationLogoParamSchema = {
   params: z.object({
-    orgId: idSchema,
+    organizationId: idSchema,
   }),
 };
