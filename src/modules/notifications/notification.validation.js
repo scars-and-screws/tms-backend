9ea779622b051d41bb2d682475752cff94a509f6
CRYP2 <faridaaa58@gmail.com>
2026-05-16 17:35:20 +0530

    add list notification query schema in nottification validation

--- src/modules/notifications/notification.validation.js
+++ src/modules/notifications/notification.validation.js
@@ -1,11 +1,17 @@
 import z from "zod";
 import { idSchema } from "../../core/validation/index.js";
-// ! NOTIFICATION ID PARAM VALIDATION SCHEMA
+import { paginationQuerySchema } from "../../core/pagination/pagination.validation.js";
 
+// ! NOTIFICATION ID PARAM VALIDATION SCHEMA
 export const notificationIdParamSchema = {
   params: z
     .object({
       notificationId: idSchema,
     })
     .strict(),
+};
+
+// ! LIST NOTIFICATIONS QUERY VALIDATION
+export const listNotificationsQuerySchema = {
+  query: paginationQuerySchema,
 };
