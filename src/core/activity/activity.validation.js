2296bdd9a6f806e2bd4a35b173abf530902295b1
CRYP2 <faridaaa58@gmail.com>
2026-05-17 21:42:28 +0530

    add project activity validation schema

---
+++ src/core/activity/activity.validation.js
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+import { idSchema } from "../validation/index.js";
+
+import { paginationQuerySchema } from "../pagination/pagination.validation.js";
+
+// ! PROJECT ACTIVITY PARAMS VALIDATION
+export const projectActivityParamsSchema = {
+  params: z
+    .object({
+      projectId: idSchema,
+    })
+    .strict(),
+};
+
+// ! PROJECT ACTIVITY QUERY VALIDATION
+export const projectActivityQuerySchema = {
+  query: paginationQuerySchema,
+};
