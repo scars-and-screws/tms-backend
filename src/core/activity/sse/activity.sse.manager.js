3e8161e6dde8266e4e61a46ed22f07b49eb7c4c8
CRYP2 <faridaaa58@gmail.com>
2026-05-26 21:56:01 +0530

    add activity sse manager

---
+++ src/core/activity/sse/activity.sse.manager.js
@@ -0,0 +1,49 @@
+// ! ORGANIZATION/PROJECT/TASK SUBCRIPTIONS
+
+const activityClients = new Map();
+
+// subscribe
+export const subscribeActivity = (scope, id, res) => {
+  const key = `${scope}:${id}`;
+
+  if (!activityClients.has(key)) {
+    activityClients.set(
+      key,
+
+      new Set()
+    );
+  }
+
+  activityClients.get(key).add(res);
+};
+
+// unsubscribe
+export const unsubscribeActivity = (scope, id, res) => {
+  const key = `${scope}:${id}`;
+
+  const clients = activityClients.get(key);
+
+  if (!clients) return;
+
+  clients.delete(res);
+
+  if (clients.size === 0) {
+    activityClients.delete(key);
+  }
+};
+
+// publish
+export const publishActivity = (scope, id, activity) => {
+  const key = `${scope}:${id}`;
+
+  const clients = activityClients.get(key);
+
+  if (!clients) return;
+
+  for (const client of clients) {
+    client.write(
+      `event:activity
+data:${JSON.stringify(activity)}\n\n`
+    );
+  }
+};
